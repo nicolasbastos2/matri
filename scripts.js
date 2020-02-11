6ce8336509045bc8d7ac1d00119f3f7bd5034574
nicolasbastos2 <39414636+nicolasbastos2@users.noreply.github.com>
2020-02-12 00:55:10 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,13 +1,15 @@
 let deferredPrompt;
-
-window.addEventListener('beforeinstallprompt', (e) => {
+    window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
   // Stash the event so it can be triggered later.
   deferredPrompt = e;
-  showInstallPromotion();
+  // Update UI notify the user they can add to home screen
+  document.getElementById("pwaa").style.display = 'block';
 });
-btnAdd.addEventListener('click', (e) => {
+    document.getElementById("pwaa").addEventListener('click', (e) => {
   // hide our user interface that shows our A2HS button
-  btnAdd.style.display = 'none';
+  document.getElementById("pwaa").style.display = 'none';
   // Show the prompt
   deferredPrompt.prompt();
   // Wait for the user to respond to the prompt
@@ -20,9 +22,6 @@ btnAdd.addEventListener('click', (e) => {
       }
       deferredPrompt = null;
     });
-});
-window.addEventListener('appinstalled', (evt) => {
-  console.log('a2hs installed');
 });
 
 (function ($) {
